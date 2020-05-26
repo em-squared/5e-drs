2960c09d3f2f32869f6c606713365706bebe86bf
Maxime Moraine <mmoraine@bearstech.com>
2020-05-26 19:30:08 +0200

    adoucissement des couleurs en mode nocturne

--- docs/.vuepress/theme/global-components/MySpellsButton.vue
+++ docs/.vuepress/theme/global-components/MySpellsButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn color="primary" class="mb-4" depressed link to="/mon-grimoire/">Mon grimoire<v-chip v-show="$store.state.mySpells.spells.length > 0" class="ml-2" color="#fff" small label>{{ $store.state.mySpells.spells.length }}</v-chip></v-btn>
+  <v-btn color="primary" class="mb-4" depressed link to="/mon-grimoire/">Mon grimoire<v-chip v-show="$store.state.mySpells.spells.length > 0" class="ml-2" color="#fff" small label text-color="#222">{{ $store.state.mySpells.spells.length }}</v-chip></v-btn>
 </template>
 
 <script>
